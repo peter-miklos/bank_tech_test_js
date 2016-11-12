a6a6f9d465ada0ee11ceb5c819c1632fb20fab9f
Peter Miklos <pt.miklos@gmail.com>
2016-11-12 20:32:06 +0000

    printStatement method and test case added

--- spec/StatementEngineSpec.js
+++ spec/StatementEngineSpec.js
@@ -9,9 +9,25 @@ describe("StatementEngine", function() {
 
   beforeEach(function() {
     statementEngine = new StatementEngine();
+    transaction1 = {
+      amount: function() { return 200 },
+      type: function() { return "credit"},
+      date: function() { return new Date("2016", "06", "11") },
+      balance: function() { return 650 }
+    }
+    transaction2 = {
+      amount: function() { return 50 },
+      type: function() { return "debit"},
+      date: function() { return new Date("2016", "08", "19") },
+      balance: function() { return 450 }
+    }
+    transactionLog = { getTransactions: function() { return [transaction1, transaction2]}}
   })
 
   describe("#printStatement", function() {
-
+    it("returns the statement with header", function() {
+      var result = "date || credit || debit || balance\n19/08/2016 || || 50.00 || 450.00\n11/06/2016 || 200.00 || || 650.00"
+      expect(statementEngine.printStatement(transactionLog)).toEqual(result);
+    })
   })
 })
--- src/StatementEngine.js
+++ src/StatementEngine.js
@@ -1,5 +1,35 @@
 "use strict"
 
 function StatementEngine() {
-  
+
+}
+
+StatementEngine.prototype = {
+  _addHeader: function() {
+    return "date || credit || debit || balance\n"
+  },
+  _addCreditAmount: function(transaction) {
+    return (transaction.type() === "credit") ? (transaction.amount().toFixed(2) + " ") : ""
+  },
+  _addDebitAmount: function(transaction) {
+    return (transaction.type() === "debit") ? (transaction.amount().toFixed(2) + " ") : ""
+  },
+  _formatDate: function(date) {
+    var month = date.getMonth() < 10 ? ("0" + date.getMonth()) : date.getMonth()
+    var day = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate()
+    return day + "/" + month + "/" + date.getFullYear()
+  },
+  _addStatementContent: function(transactionLog) {
+    var transactions = transactionLog.getTransactions();
+    transactions = transactions.sort(function(a, b) { return b.date() - a.date()})
+    var self = this
+    transactions = transactions.map(function(tr) {
+      return self._formatDate(tr.date()) + " || " + self._addCreditAmount(tr) + "|| " + self._addDebitAmount(tr) + "|| " + tr.balance().toFixed(2)
+    }).join("\n")
+    return transactions
+  },
+  printStatement: function(transactionLog) {
+    var statement = this._addHeader() + this._addStatementContent(transactionLog)
+    return statement
+  }
 }
