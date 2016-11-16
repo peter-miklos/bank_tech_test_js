22799766bde35f30aef24a6d31a3b2b75c623af4
Peter Miklos <pt.miklos@gmail.com>
2016-11-16 22:16:29 +0000

    statementEngine updated, class added

--- src/StatementEngine.js
+++ src/StatementEngine.js
@@ -1,38 +1,37 @@
 "use strict"
 
-function StatementEngine() {
-}
-
-StatementEngine.prototype = {
-  _addHeader: function() {
+class StatementEngine{
+  constructor() {
+  }
+  _addHeader() {
     return "date || credit || debit || balance\n"
-  },
-  _addCreditAmount: function(transaction) {
+  }
+  _addCreditAmount(transaction) {
     return (transaction.type() === "credit") ? (transaction.amount().toFixed(2) + " ") : ""
-  },
-  _addDebitAmount: function(transaction) {
+  }
+  _addDebitAmount(transaction) {
     return (transaction.type() === "debit") ? (transaction.amount().toFixed(2) + " ") : ""
-  },
-  _formatDate: function(date) {
+  }
+  _formatDate(date) {
     var month = date.getMonth() < 10 ? ("0" + date.getMonth()) : date.getMonth()
     var day = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate()
     return day + "/" + month + "/" + date.getFullYear()
-  },
-  _getSortedTransactions: function(transactionLog) {
+  }
+  _getSortedTransactions(transactionLog) {
     transactionLog = transactionLog.getTransactions();
     return transactionLog.sort(function(a, b) { return b.date() - a.date()})
-  },
-  _getTransactionsInString: function(transactions, self) {
+  }
+  _getTransactionsInString(transactions, self) {
     return transactions.map(function(tr) {
       return self._formatDate(tr.date()) + " || " + self._addCreditAmount(tr) + "|| " + self._addDebitAmount(tr) + "|| " + tr.balance().toFixed(2)
     }).join("\n")
-  },
-  _addStatementContent: function(transactionLog) {
+  }
+  _addStatementContent(transactionLog) {
     var self = this;
     var transactions = self._getSortedTransactions(transactionLog)
     return self._getTransactionsInString(transactions, self)
-  },
-  printStatement: function(transactionLog) {
+  }
+  printStatement(transactionLog) {
     var statement = this._addHeader() + this._addStatementContent(transactionLog)
     return statement
   }
